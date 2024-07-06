3d675e5058e49ae4241ecb429503cfdfa69f3629
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-06 17:29:25 -0500

    Create vendedores.js

---
+++ plugins/vendedores.js
@@ -0,0 +1,44 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.vendedores);
+};
+handler.command = /^(vendedores)$/i;
+export default handler;
+global.vendedores = `
+*VENDEDORES DE : 𝙋𝙍𝙊𝙔𝙀𝘾𝙏𝙊 𝙍.𝘼 🕷️*
+
+*👤Ig :* https://www.instagram.com/proyecto_r.a_
+*🇪🇨 RICARDO CEO 
+Contact :* https://wa.me/593979133620
+
+*👤 Ig :* https://www.instagram.com/proyecto_r.a_
+*🇩🇴 ANTHONY CEO
+ Contact :* https://wa.me/50764206082
+
+*👤 Ig :* https://www.instagram.com/proyecto_r.a_
+*🇪🇨 ELI CO-CEO
+Contact :* https://wa.me/593983025939
+
+*👤 Ig :* https://www.instagram.com/proyecto_r.a_
+*🇲🇽 ALE CO-CEO
+Contact :* https://wa.me/+529991091929
+
+*👤 Ig :* https://www.instagram.com/proyecto_r.a_
+*🇪🇨 ELI CO-CEO
+Contact :* https://wa.me/593983025939
+
+*👤 Ig :* https://www.instagram.com/proyecto_r.a_
+*🇲🇽 ALE CO-CEO
+Contact :* https://wa.me/+529991091929
+
+*👤 Ig :* https://www.instagram.com/proyecto_r.a_
+*🇻🇪 SEMI CO-CEO
+ Contact :* https://wa.me/+584262668729
+
+𝗣𝗼𝗿 𝗦𝗶 𝗗𝗲𝘀𝗲𝗮𝘀 𝗖𝗼𝗺𝗽𝗿𝗮𝗿 𝗕𝗼𝘁 𝗣𝗿𝗼𝗽𝗶𝗼 , 𝗚𝗿𝘂𝗽𝗼 𝗢 𝗛𝗮𝗰𝗲𝗿 𝗖𝗼𝗹𝗮𝗯𝗼𝗿𝗮𝗰𝗶𝗼𝗻 
+
+*🇪🇨 RICARDO CREADOR
+Contact :* https://wa.me/593979133620
+
+*🇩🇴 ANTHONY CREADOR
+ Contact :* https://wa.me/50764206082
+`;
