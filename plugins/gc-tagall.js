fed7122661ff440c589bbf4bb964dffba4a20956
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-06 17:32:51 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -10,11 +10,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join``;
   const oi = `${pesan}`;
-  let teks = `╭─────────\n│❏ 𝗗𝗿𝗮𝗴𝗼𝗻 𝗕𝗼𝘁 🐉\n│❏ 𝗠𝗶𝗲𝗺𝗯𝗿𝗼𝘀 : *${participants.length}* ${oi}\n│❏ 𝗩𝗲𝗻𝘁𝗮 𝗗𝗲 𝗕𝗼𝘁 𝗔𝗹 +51 926 933 818\n│\n`;
+  let teks = `╭─────────\n│❏ Spider 𝗕𝗼𝘁 🐉\n│❏ 𝗠𝗶𝗲𝗺𝗯𝗿𝗼𝘀 : *${participants.length}* ${oi}\n│❏ 𝘼𝙘𝙩𝙞𝙫𝙖𝙩𝙚 𝙢𝙖𝙣𝙘@\n│\n`;
   for (const mem of participants) {
     teks += `│🐉 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `│\n╰@𝗖𝗿𝗶𝘀𝘁𝗵𝗼𝗳𝗲𝗿_𝗬𝗮𝗹𝗹𝗶𝗰𝗼`;
+  teks += `│\n╰@proyecto_r.a_`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
@@ -23,4 +23,3 @@ handler.command = /^(tagall|invocar|marcar|todos|invocación|ta)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
-
